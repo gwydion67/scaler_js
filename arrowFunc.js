234f94c53e8d79747d73e925c97a9d2e00f6c9f6
Gwydion <gwydionmaranio@gmail.com>
2023-03-02 18:27:49 +0530

    new files

---
+++ arrowFunc.js
@@ -0,0 +1,16 @@
+let a = 2;
+
+let test1 = function(){
+    console.log(a+2);
+}
+
+let test2 = () => {
+    console.log(4);
+}
+
+let test3 = a => {
+    console.log(a+5);
+}
+
+let test4 = (a,b) => console.log(a+b);
+
